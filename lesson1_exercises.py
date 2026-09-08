45a6a3c426935a4e83bf1affd7f35af701f6e2df
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-08 15:48:28 +0200

    Three variables

--- lesson1_exercises.py
+++ lesson1_exercises.py
@@ -2,4 +2,8 @@ print("Martin Pettersson")
 print("Lexicon Python and AI")
 print("Today's study goal is to learn how to use Python for AI applications.")
 
+name = "Martin Pettersson"
+height = 1.89
+student = True
+
 
