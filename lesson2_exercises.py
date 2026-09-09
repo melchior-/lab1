24d51a44ad7ab59287dba37ffe7ece7cd0cdcbe1
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-09 12:40:55 +0200

    Added lesson2. Added list of programming languages

---
+++ lesson2_exercises.py
@@ -0,0 +1,6 @@
+languages = ["Python", "JavaScript", "C++", "Java", "Ruby", "Go", "Swift", "Kotlin", "PHP", "TypeScript"]
+first_language = languages[0]
+last_language = languages[-1]
+third_language = languages[2]
+second_to_last_language = languages[-2]
+
