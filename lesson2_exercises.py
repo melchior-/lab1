a9ffd30ad0c644f3fbc3a781ce1cd86fc8e06c62
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-09 12:45:50 +0200

    Operations

--- lesson2_exercises.py
+++ lesson2_exercises.py
@@ -7,3 +7,13 @@ second_to_last_language = languages[-2]
 print(languages[3:6])
 print(languages[::2])
 print(languages[::-1])
+
+languages.append("Rust")
+print(languages)
+languages.insert(2, "C#")
+print(languages)
+languages.remove("Java")
+print(languages)
+languages.pop(4)
+print(languages)
+
