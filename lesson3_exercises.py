e9072f6df8b602515de1450cae5907e79b294370
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-10 15:38:01 +0200

    Score

--- lesson3_exercises.py
+++ lesson3_exercises.py
@@ -1,4 +1,4 @@
-num = 3
+num = float(input("Enter a number: "))
 if num > 0:
     print("Positive number")
 elif num < 0:
@@ -22,4 +22,16 @@ input_username = input("Enter your username: ")
 input_password = input("Enter your password: ")
 if input_username == username and input_password == password:
     print("Login successful!")
+
+score = 78
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
